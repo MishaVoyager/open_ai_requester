bcd1b2b2d968d16cb300119185093205f8cf9362
Noskov Mikhail <mnoskov@skbkontur.ru>
2025-02-04 10:53:02 +0500

    Фикс измерения времени

--- helpers/timehelper.py
+++ helpers/timehelper.py
@@ -1,6 +1,21 @@
 import logging
 import time
 
+from functools import wraps
+
+
+def measure_time_async(async_func):
+    @wraps(async_func)
+    async def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = await async_func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        logging.info(f'Функция "{async_func.__name__}" выполнена за {execution_time:.4f} секунд.')
+        return result
+
+    return wrapper
+
 
 def measure_time(func):
     def wrapper(*args, **kwargs):
